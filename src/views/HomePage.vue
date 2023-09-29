2bcc7cea352d795262f57c7a0799966b2c5cacc2
Samuel Yuma K <samuelyuma.117@gmail.com>
2023-09-29 15:50:57 +0700

    removing some components, fixing name input

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -5,13 +5,23 @@
         type="text"
         placeholder="Your name here"
         v-model="name"
-        class="py-3 px-2 border-b-2 border-gray-800 mr-4 active:border-0 focus:border-0"
+        class="py-2 px-2 border-b-2 border-slate-800 mr-4 focus:outline-none text-4xl font-bold w-3/12 text-slate-800"
       />
-      <h2 class="font-exo font-bold text-gray-800 text-4xl">'s To-Do-List</h2>
+      <h2 class="font-bold text-slate-800 text-4xl py-2">'s To-Do-List</h2>
     </section>
     <section class="flex flex-col gap-5">
       <h3 class="mt-12 font-medium text-4xl">Add new categories:</h3>
-      <AddComponent />
+      <div class="flex gap-4">
+        <input
+          type="text"
+          class="w-auto border-2 border-slate-400 rounded-xl bg-slate-200 font-medium text-xl text-slate-600 px-3 py-2 focus:ring-0 focus:border-transparent focus:outline-slate-400"
+        />
+        <button
+          class="text-white font-medium text-xl px-12 py-2 rounded-xl bg-purple-500"
+        >
+          Add
+        </button>
+      </div>
     </section>
 
     <h3 class="mt-12 mb-7 font-medium text-4xl">Categories:</h3>
@@ -24,12 +34,10 @@
 
 <script>
 import CategoryCard from '../components/CategoryCard.vue';
-import AddComponent from '../components/AddComponent.vue';
 
 export default {
   components: {
     CategoryCard,
-    AddComponent,
   },
   data: () => {
     return {
