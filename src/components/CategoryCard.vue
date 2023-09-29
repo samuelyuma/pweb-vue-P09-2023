644ef90e785761ceab9f3136d53aadf6535c929e
Samuel Yuma K <samuelyuma.117@gmail.com>
2023-09-29 14:04:44 +0700

    adding category card component

---
+++ src/components/CategoryCard.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="w-full shadow-xl pb-full rounded-3xl bg-blue-500 px-9 py-7">
+    <div class="flex flex-col gap-40">
+      <h1 class="text-white text-3xl font-semibold">College</h1>
+      <div class="flex items-center justify-between">
+        <p class="text-white text-xl font-medium">... notes</p>
+        <button
+          class="text-blue-500 bg-white font-semibold text-4xl rounded-full pb-2 px-3"
+        >
+          +
+        </button>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    categories: String,
+  },
+};
+</script>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,34 @@
+<template>
+  <main class="mx-32 my-44">
+    <section class="flex">
+      <input
+        type="text"
+        placeholder="Your name here"
+        v-model="name"
+        class="py-3 px-2 border-b-2 border-gray-800 mr-4 active:border-0 focus:border-0"
+      />
+      <h2 class="font-exo font-bold text-gray-800 text-4xl">'s To-Do-List</h2>
+    </section>
+    <section class="grid grid-cols-4 gap-10 mt-16">
+      <CategoryCard />
+      <CategoryCard />
+      <CategoryCard />
+      <CategoryCard />
+    </section>
+  </main>
+</template>
+
+<script>
+import CategoryCard from '../components/CategoryCard.vue';
+
+export default {
+  components: {
+    CategoryCard,
+  },
+  data: () => {
+    return {
+      name: '',
+    };
+  },
+};
+</script>
